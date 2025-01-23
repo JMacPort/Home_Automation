79f05d1fc444f8441dd4045d7076c574decfc6fe
jmac1 <145376972+JMacPort@users.noreply.github.com>
2025-01-23 11:29:17 -0500

    Basic button configuration

--- Drivers/Peripherals/Include/button.h
+++ Drivers/Peripherals/Include/button.h
@@ -1,6 +1,11 @@
 #ifndef PERIPERALS_BUTTON_H_
 #define PERIPERALS_BUTTON_H_
 
+#define GPIO_PORT_A	0
+#define BUTTON_PIN  9
+#define BUTTON_MODE	0		// Input
+
+void Button_Init(void);
 
 
 #endif /* PERIPERALS_BUTTON_H_ */
--- Drivers/Peripherals/Src/button.c
+++ Drivers/Peripherals/Src/button.c
@@ -1,2 +1,12 @@
+#include "main.h"
+#include "button.h"
 
+void Button_Init() {
+	RCC -> APB1ENR |= (1 << GPIO_PORT_A);
+
+	GPIOA -> MODER &= ~(3 << (2 * BUTTON_PIN));
+
+	GPIOA -> PUPDR &= ~(3 << (2 * BUTTON_PIN));
+	GPIOA -> PUPDR |= (1 << (2 * BUTTON_PIN));
+}
 
