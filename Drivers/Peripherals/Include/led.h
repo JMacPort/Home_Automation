40ce53d65e5a190b15ecc257c1b372a32b499415
jmac1 <145376972+JMacPort@users.noreply.github.com>
2025-01-23 11:17:28 -0500

    LED Interface created with function pointers to turn LED on and off.

--- Drivers/Peripherals/Include/led.h
+++ Drivers/Peripherals/Include/led.h
@@ -1,6 +1,20 @@
 #ifndef PERIPERALS_LED_H_
 #define PERIPERALS_LED_H_
 
+#define GPIO_PORT_A	0
+#define LED_PIN		8
+#define LED_MODE	1		// Output
 
+void Led_Init(void);
+
+// Function pointer to create an LED interface if multiple LEDs were to be used
+typedef void (*ledFunc) (void);
+
+typedef struct {
+	const ledFunc On;
+	const ledFunc Off;
+} led;
+
+extern led MyLED;
 
 #endif /* PERIPERALS_LED_H_ */
---
+++ Drivers/Peripherals/Src/led.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "led.h"
+
+void LED_Init() {
+	RCC -> APB1ENR |= (1 << GPIO_PORT_A);
+
+	GPIOA -> MODER &= ~(LED_MODE << LED_PIN);
+	GPIOA -> MODER |= (LED_MODE << LED_PIN);
+}
+
+void LED_ON() {GPIOA -> ODR |= (1 << LED_PIN);}
+
+void LED_OFF() {GPIOA -> ODR &= ~(0 << LED_PIN);}
+
+led MyLED = {LED_ON, LED_OFF};
